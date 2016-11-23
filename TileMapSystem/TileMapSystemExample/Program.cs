a06f981d73f27d8e87f908cf9b0d170cfa739558
crone66 <marcel.croonenbroeck@gmail.com>
2016-11-23 18:16:45 +0100

    added performance test and logging, implemented TileMapManager

--- TileMapSystem/TileMapSystemExample/Program.cs
+++ TileMapSystem/TileMapSystemExample/Program.cs
@@ -11,73 +11,134 @@ namespace TileMapMangerExample
     {
         static void Main(string[] args)
         {
-            Stopwatch watch = new Stopwatch();
-            watch.Start();
-            TileMapGenerator generator = new TileMapGenerator();
-            StreamedTileMap map = generator.GenerateMap(new GeneratorSettings(1, 50, 1.5f, 1000000, 10000000, true, 1000f, LayerDepth.One),
-                new AreaSpread[2] { new AreaSpread(1, 0.30f, 0, 20, 250, true, SpreadOption.Circle), new AreaSpread(2, 0.125f, 0, 20, 200, true, SpreadOption.Circle) }, 1, 1);
-            watch.Stop();
-            double seconds = watch.Elapsed.TotalSeconds;
-
-            map.Update(1, 1);
-            int[,] screenMap = map.GetTileMapInScreen(800, 600);
-            SaveToFile(screenMap, "screenMap");
-
-
-            watch.Restart();
-            int counter = 0;
-            for (int k = 0; k < 9; k++)
+            double sum = 0;
+            double sum2 = 0;
+            double sum3 = 0;
+            long inUse = 0;
+            int iterations = 10;
+            int updateIterations = 10;
+            TileMapManager tileMapManager;// = new TileMapManager(new GeneratorSettings(1, 50, 1.5f, 1000000, 10000000, true, 1000f),
+            //        new AreaSpread[2] { new AreaSpread(1, 0.30f, 0, 20, 250, true, SpreadOption.Circle, LayerType.Height), new AreaSpread(2, 0.125f, 0, 20, 200, true, SpreadOption.Circle, LayerType.Height) }, 1, 1);
+            for (int b = 0; b < iterations; b++)
             {
-                counter += SaveToFile(map.Maps[k].MapSurface, "map" + k.ToString());
+                Stopwatch watch = new Stopwatch();
+                watch.Start();
+                //TileMapGenerator generator = new TileMapGenerator();
+                //StreamedTileMap map = generator.GenerateMap(new GeneratorSettings(1, 50, 1.5f, 1000000, 10000000, true, 1000f),
+                //    new AreaSpread[2] { new AreaSpread(1, 0.30f, 0, 20, 250, true, SpreadOption.Circle, LayerType.Height), new AreaSpread(2, 0.125f, 0, 20, 200, true, SpreadOption.Circle, LayerType.Height) }, 1, 1);
+                tileMapManager = new TileMapManager(new GeneratorSettings(1, 50, 1.5f, 1000000, 10000000, true, 1000f),
+                    new AreaSpread[2] { new AreaSpread(1, 0.30f, 0, 20, 250, true, true, 5, SpreadOption.Circle, LayerType.Height), new AreaSpread(2, 0.125f, 0, 20, 200, true, true, 5, SpreadOption.Circle, LayerType.Height) }, 1, 1);
+                StreamedTileMap map = tileMapManager.CurrentLevel;
+                watch.Stop();
+                double seconds = watch.Elapsed.TotalSeconds;
+
+                tileMapManager.Update(1, 1);
+                byte[] screenMap = map.GetTileMapInScreen(800, 600);
+                SaveToFile(screenMap, "screenMap"+b.ToString(), 800 / 50);
+
+
+                watch.Restart();
+                int counter = 0;
+                for (int k = 0; k < 9; k++)
+                {
+                    counter += SaveToFile(map.Maps[k].MapSurface, "map" + b.ToString() + "_" + k.ToString(), map.TileColumnCount);
+                }
+
+                watch.Stop();
+
+                watch.Restart();
+                tileMapManager.Update(-1, -1);
+                watch.Stop();
+                double update1 = watch.Elapsed.TotalSeconds;
+                Thread.Sleep(5000); //wait for generation thread (only for debugging)
+
+
+                watch.Restart();
+                tileMapManager.Update(-1, -1);
+                watch.Stop();
+                double update2 = watch.Elapsed.TotalSeconds;
+
+                for (int k = 0; k < 9; k++)
+                {
+                    counter += SaveToFile(map.Maps[k].MapSurface, "map2_" + b.ToString() + "_" + k.ToString(), map.TileColumnCount);
+                }
+
+                Log("Tiles generated: " + counter.ToString());
+                Log("Generation time: " + seconds.ToString() + " seconds");
+                Log("Write time: " + watch.Elapsed.TotalSeconds.ToString() + " seconds");
+                Log("Update1 time: " + update1.ToString() + " seconds");
+                Log("Update2 time: " + update2.ToString() + " seconds");
+
+                for (int i = 0; i < updateIterations; i++)
+                {
+                    watch.Restart();
+                    tileMapManager.Update(-1 - i, -1 - i);
+                    byte[] data = map.GetTileMapInScreen(800, 600);
+                    watch.Stop();
+                    update2 = watch.Elapsed.TotalMilliseconds;
+                    sum += update2;
+                    Log("Update time: " + update2.ToString() + " ms");
+                    inUse += Process.GetCurrentProcess().PrivateMemorySize64;
+                    Log("MemoryInUse: " + Process.GetCurrentProcess().PrivateMemorySize64.ToString() + " byte");
+                }
+
+                watch.Restart();
+                tileMapManager.Update(1, 1);
+                watch.Stop();
+                update2 = watch.Elapsed.TotalMilliseconds;
+                sum2 += update2;
+                Log("Update time: " + update2.ToString() + " ms");
+                Thread.Sleep(2000);
+                watch.Restart();
+                tileMapManager.Update(1, 1);
+                watch.Stop();
+                update2 = watch.Elapsed.TotalMilliseconds;
+                sum3 += update2;
+                Log("Update time: " + update2.ToString() + " ms");
+                Log("Done");
+
             }
-
-            watch.Stop();
-
-            watch.Restart();
-            map.Update(-1, -1);
-            watch.Stop();
-            double update1 = watch.Elapsed.TotalSeconds;
-            Thread.Sleep(5000); //wait for generation thread (only for debugging)
-
-
-            watch.Restart();
-            map.Update(-1, -1);
-            watch.Stop();
-            double update2 = watch.Elapsed.TotalSeconds;
-
-            for (int k = 0; k < 9; k++)
-            {
-                counter += SaveToFile(map.Maps[k].MapSurface, "map2_" + k.ToString());
-            }
-
-            Console.WriteLine("Tiles generated: " + counter.ToString());
-            Console.WriteLine("Generation time: " + seconds.ToString() + " seconds");
-            Console.WriteLine("Write time: " + watch.Elapsed.TotalSeconds.ToString() + " seconds");
-            Console.WriteLine("Update1 time: " + update1.ToString() + " seconds");
-            Console.WriteLine("Update2 time: " + update2.ToString() + " seconds");
-            Console.WriteLine("Done");
+            Console.Clear();
+            Log("Iterations: " + iterations.ToString());
+            Log("UpdateIterations: " + (iterations * updateIterations).ToString());
+            Log("Avg Update Time: " + (sum / (iterations * updateIterations)).ToString() + " ms");
+            Log("Avg Update Time (Pre GridChange):" + (sum2 / iterations).ToString() + " ms");
+            Log("Avg Update Time (GridChange):" + (sum3 / iterations).ToString() + " ms");
+            Log("Avg MemoryInUse: " +(inUse / (iterations * updateIterations)).ToString() + " byte");
+            Log("Final MemoryInUse: " + Process.GetCurrentProcess().PrivateMemorySize64.ToString() + " byte");
             Console.ReadKey();
         }
 
-        private static int SaveToFile(int[,] map, string fileName)
+        private static int SaveToFile(byte[] map, string fileName, int columnCount)
         {
-            int row = map.GetUpperBound(0);
-            int col = map.GetUpperBound(1);
-            int counter = row * col;
+            int counter = -1;
             List<string> lines = new List<string>();
             StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < row; i++)
+            for (int i = 0; i < map.Length; i++)
             {
-                for (int j = 0; j < col; j++)
+                int mod = (i % columnCount);
+                if (mod == 0)
                 {
-                    sb.Append(map[i, j]);
+                    counter++;
+                    if (sb.Length > 0)
+                    {
+                        lines.Add(sb.ToString());
+                        sb.Clear();
+                    }
                 }
-                lines.Add(sb.ToString());
-                sb.Clear();
+
+                sb.Append(Convert.ToInt32(map[(counter * columnCount) + mod]));
             }
 
             System.IO.File.WriteAllLines(fileName + ".txt", lines.ToArray());
-            return counter;
+            return map.Length;
+        }
+
+        private static string path = "log.txt";
+        private static void Log(string text)
+        {
+            Console.WriteLine(text);
+            System.IO.File.AppendAllText(path, text + Environment.NewLine);
         }
     }
 }
